ce993730cd904ce815deccf8015b3947d19f1896
Brett Doyle <brettdoyle@protonmail.com>
2021-01-11 21:45:43 +0000

    Updated

--- src/routes/frame/control.svelte
+++ src/routes/frame/control.svelte
@@ -1,4 +1,6 @@
 <script>
+    import { fly } from 'svelte/transition';
+
     const actions = [
         {
             icon: 'power-off',
@@ -60,7 +62,12 @@
     ];
 
     const system = {
-        rotation: 'normal'
+        rotation: 'normal',
+
+        temp: 0,
+
+        safeTemp: 50,
+        unsafeTemp: 70
     };
 
     const rotations = [
@@ -82,48 +89,13 @@
         },
     ];
 
-    let temp = 0;
+    let frame = {
+        url: '',
+        paused: false
+    };
 
-    let url = '';
 
-    let presets = [
-        {
-            url: 'http://www.fallingfalling.com/'
-        },
-        {
-            url: 'https://graphics.brettdoyle.art/glsl/color/'
-        },
-        {
-            url: 'https://graphics.brettdoyle.art/canvas/bml-traffic/'
-        },
-        {
-            url: 'https://graphics.brettdoyle.art/canvas/rps/'
-        },
-        {
-            url: 'https://graphics.brettdoyle.art/canvas/rps/3'
-        },
-        {
-            url: 'https://graphics.brettdoyle.art/canvas/rps/5'
-        },
-        {
-            url: 'https://graphics.brettdoyle.art/canvas/rps/rgb'
-        },
-        {
-            url: 'https://www.www.www.brettdoyle.art/lines.html'
-        },
-        {
-            url: 'https://www.www.www.brettdoyle.art/arches.html'
-        },
-        {
-            url: 'https://www.www.www.brettdoyle.art/langtonsAnt.html'
-        },
-        {
-            url: 'https://www.www.www.brettdoyle.art/life_3.html'
-        },
-        {
-            url: 'https://www.www.www.brettdoyle.art/head.html'
-        },
-    ];
+    let presets = [];
 
     function setRotation(rotation) {
         if (typeof Window !== 'undefined') {
@@ -135,36 +107,85 @@
     
     }
 
-    function setUrl() {
+    function setPaused(paused) {
+        frame.paused = paused;
+
         if (typeof Window !== 'undefined') {
             Window.socket.send('frame', {
-                url: url
+                paused: frame.paused
+            });
+        }
+    }
+
+    function setUrl(url) {
+        frame.url = url;
+
+        if (typeof Window !== 'undefined') {
+            Window.socket.send('frame', {
+                url: frame.url
+            });
+        }
+    }
+
+    function addPreset(url) {
+
+        presets.push({ url: frame.url });
+
+        if (typeof Window !== 'undefined') {
+            Window.socket.send('storage-set', {
+                key: 'presets',
+                value: presets
             });
         }
     }
 
     function presetClick() {
-        url = this.getAttribute('data-url');
-
-        setUrl();
+        setUrl(this.getAttribute('data-url'));
     }
 
     function onInputKey(event) {
         if (event.code.toLowerCase() === 'enter') {
-            setUrl();
+            setUrl(frame.url);
         }
     }
 
     if (typeof Window !== 'undefined') {
         Window.socket.on('stats', function(data) {
-            temp = Math.round(data.temp / 1000);
+            system.temp = Math.round(data.temp / 1000);
+        });
+
+        Window.socket.on('frame', function(data) {
+            if (typeof(data.url) === 'string') {
+                frame.url = data.url;
+            }
+
+            if (typeof(data.paused) === 'string') {
+                frame.paused = data.paused;
+            }
         });
 
         Window.socket.on('system', function(data) {
             system.rotation = data.rotation;
         });
 
+        Window.socket.on('storage-get', function(data) {
+            switch (data.key) {
+                case 'presets':
+                    presets = data.value;
+                    break;
+                
+                case 'system.unsafeTemp':
+                    system.unsafeTemp = data.value;
+                    break;
+                
+                case 'system.safeTemp':
+                    system.safeTemp = data.value;
+                    break;
+            }
+        })
+
         Window.socket.send('system', { action: 'get-rotation' });
+        Window.socket.send('storage-get', { key: 'presets', value: presets });
     }
 </script>
 
@@ -216,8 +237,18 @@
 
     .header .actions {
         flex: 1 1 auto;
+
+        margin-bottom: -1em;
     }
     .header .actions button {
+        margin-right: 1em;
+        margin-bottom: 1em;
+    }
+    .header .actions .seperator {
+        height: 100%;
+
+        border-right: 1px solid black;
+        
         margin-right: 1em;
     }
 
@@ -251,6 +282,16 @@
                 {/if}
             {/each}
 
+            <span class="seperator"></span>
+
+            {#if frame.paused}
+                <button on:click={() => {setPaused(false)}}><i class="fas fa-pause-circle fa-fw"></i></button>
+                {:else}
+                <button class="inactive" on:click={() => {setPaused(true)}}><i class="fas fa-pause-circle fa-fw"></i></button>
+            {/if}
+
+            <span class="seperator"></span>
+
             {#each rotations as option}
                 {#if option.rotation === system.rotation}
                     <button on:click={() => {setRotation(option.rotation)}}><i class="fas fa-{option.icon} fa-fw"></i></button>
@@ -258,21 +299,30 @@
                     <button class="inactive" on:click={() => {setRotation(option.rotation)}}><i class="fas fa-{option.icon} fa-fw"></i></button>
                 {/if}
             {/each}
+
+            <!-- TODO: Add pointer toggle - enable / disable pointer-events: none; -->
         </div>
-        <p class="info">{temp}°</p>
+        <p class="info">{system.temp}°</p>
     </div>
 
     <div class="main">
-        <input type="url" bind:value={url} on:keyup={onInputKey}>
-        <button on:click={setUrl}>SET</button>
+        <input type="url" bind:value={frame.url} on:keyup={onInputKey}>
+        <button on:click={() => {setUrl(frame.url)}}><i class="fas fa-sign-in-alt fa-fw"></i></button>
+        <button on:click={addPreset}><i class="fas fa-cloud-download-alt fa-fw"></i></button>
     </div>
     
     <div class="presets">
-        {#each presets as preset}
+        {#each presets as preset, i}
             {#if preset.name}
-                <button on:click={presetClick} data-url={preset.url}><span>{preset.name}</span></button>
+                <button
+                    in:fly="{{  y: -50, duration: 100, delay: i * 100 }}"
+                    out:fly="{{ y: 50, duration: 200, delay: i * 100 }}"
+                    on:click={presetClick} data-url={preset.url}><span>{preset.name}</span></button>
             {:else}
-                <button on:click={presetClick} data-url={preset.url}><span>{preset.url}</span></button>
+                <button
+                    in:fly="{{  y: -50, duration: 100, delay: i * 100 }}"
+                    out:fly="{{ y: 50, duration: 200, delay: i * 100 }}"
+                    on:click={presetClick} data-url={preset.url}><span>{preset.url}</span></button>
             {/if}
         {/each}
     </div>
